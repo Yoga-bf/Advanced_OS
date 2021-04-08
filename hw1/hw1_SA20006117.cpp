0dff8272b499e67d9f435b8341a62748d94f6e8c
CBF98 <cbf98012@mail.ustc.edu.cn>
2021-04-07 21:35:25 -0700

    hw1_test

---
+++ hw1/hw1_SA20006117.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <mpi.h>
+
+using namespace std;
+
+int main (int argc, char **argv)
+{
+    int myid, numprocs;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    cout << "Hello" << myid << endl;
+    MPI_Finalize();
+    return 0;
+}
